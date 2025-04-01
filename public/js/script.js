b4656c60881764710763c2c0d437a9251a31671a
Mawuli2026 <mawuliayikpa89@gmail.com>
2025-04-01 14:28:05 +0100

    Added changes to styles

--- public/js/script.js
+++ public/js/script.js
@@ -35,3 +35,8 @@ document.addEventListener("DOMContentLoaded", () => {
         });
     });
 });
+
+function toggleMenu() {
+    document.getElementById("nav-menu").classList.toggle("active");
+  }
+  
